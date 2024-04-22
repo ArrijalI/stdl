b2d4bcfac2d1e3ad531b7598675c1ed2b7149c8f
ArrijalI <ignisnero228@gmail.com>
2024-04-22 14:03:17 +0700

    add browser icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('images/logo-stdl-dark.png') }}" type="image/png">
     <title>STDL</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
