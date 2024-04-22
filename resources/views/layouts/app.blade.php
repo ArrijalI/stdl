16866b3ed54a6f89ae7636747bd4bea6e6dc1a34
ArrijalI <ignisnero228@gmail.com>
2024-04-22 17:52:05 +0700

    change script directories

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="{{ asset('images/logo-stdl-dark.png') }}" type="image/png">
     <title>STDL</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BZfNq5Ls.css') }}">
+    <script src="{{ asset('build/assets/app-C5w12ZIH.js') }}"></script>
 </head>
 
 <body class="dark:bg-gray-800 dark:border-gray-700">
